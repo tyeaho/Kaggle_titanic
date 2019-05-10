1880ce5e9adb6042b9712109c113ba081c62c0b6
tyeaho <50167487+tyeaho@users.noreply.github.com>
2019-05-10 21:17:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,73 @@
 
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from pandas.plotting import scatter_matrix
+
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.metrics import accuracy_score, log_loss
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+
+pd.set_option('display.width',3200)
+pd.set_option('display.max_columns',1000)
+
+
+train = pd.read_csv('~/Kaggle/titanic_2/data/train.csv')
+test = pd.read_csv('~/Kaggle/titanic_2/data/test.csv')
+
+
+
+train_X=train.drop('Survived',axis=1)
+train_Y=train['Survived']
+
+merged=pd.concat([train_X,test],ignore_index=True)
+
+merged=merged.drop(['Name','PassengerId','Ticket','Cabin'],axis=1)
+print(merged.info())
+
+Train_new = merged[0:891]
+Test_new = merged[891:merged.shape[0]]
+X_train, X_test, y_train, y_test = train_test_split(train_X, train['Survived'], test_size=0.2)
+
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', StandardScaler())])
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))])
+
+numeric_features =['Age','Fare']
+
+categorical_features =['Sex','Embarked']
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features),
+        ('cat', categorical_transformer, categorical_features)])
+
+classifiers = [
+    KNeighborsClassifier(3),
+    SVC(kernel="rbf", C=0.025, probability=True),
+    NuSVC(probability=True),
+    DecisionTreeClassifier(),
+    RandomForestClassifier(),
+    AdaBoostClassifier(),
+    GradientBoostingClassifier()
+]
+
+for classifier in classifiers:
+    pipe = Pipeline(steps=[('preprocessor', preprocessor),
+                      ('classifier', classifier)])
+    pipe.fit(X_train, y_train)
+    print(classifier)
+    print("model score: %.3f" % pipe.score(X_test, y_test))
